2a5093423dc0b41f4d2b146dede6de6a01ad99a0
G_C <alangong91014@gmail.com>
2018-02-11 17:12:30 +0800

    update

--- ConfigServer/src/main/java/com/example/config/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/example/config/ConfigServerApplication.java
@@ -1,14 +1,20 @@
 package com.example.config;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaClient
+public class ConfigServerApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
